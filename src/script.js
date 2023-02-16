bdc93b0cdaab98a880f3f13b86bb11fefc71abcf
hihinkoland <kamil.slimak.sk@gmail.com>
2023-02-16 21:49:04 +0100

    Added content to both pages. Modified how the spinner is positioned and scaled. Loading dots now have unbreakable spaces at the end to stay relatively the same width.

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // Animate ... dots
 let dotSpans = document.getElementsByClassName('loadingDots');
-let possibleDots = [".", "..", "...", "....", "....."];
+let possibleDots = [".\u2003", "..\u2000\u2005", "...\u2000", "....\u00a0", "....."];
 var dotIndex = 0;
 animateDots();
 function animateDots() {
@@ -13,3 +13,14 @@ function animateDots() {
 		dotIndex++;
 	setTimeout(animateDots, 1000);
 }
+
+// Display opening hours of today
+let hrs = document.getElementById('hrs-actual');
+const date = new Date();
+let day = date.getDay();
+if (day <= 5 && day >= 1)
+	hrs.innerHTML = '<i>8:00 - 18:00</i>';
+else if (day == 6)
+	hrs.innerHTML = '<i class="accent">9:00 - 12:00</i>';
+else
+	hrs.innerHTML = '<b><i class="danger"> CLOSED &ensp;</i></b>';
